e911ef1e7a43598b8c55c0a2d2fa5a0b3f510529
Artem <d00m7@ukr.net>
2018-08-21 13:25:55 +0300

    Template first install

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::group(['namespace' => 'User'], function () {
+
+    Route::get('/', 'HomeController@index')->name('user.index');
+    /*Route::get('/about', 'AboutController@index')->name('about');
+    Route::any('/category/{category}', 'CategoryController@index')->name('category');
+    Route::get('/cooperation', 'CooperationController@index')->name('cooperation');
+    Route::get('/delivery', 'DeliveryController@index')->name('delivery');
+    Route::get('/contact/{product?}', 'ContactController@index')->name('contact');
+    Route::get('/payment', 'PaymentController@index')->name('payment');
+    Route::get('/product/{product}', 'ProductController@index')->name('product');
+    Route::get('/search','SearchController@search')->name('search');
+    Route::post('/sendMail/{slug?}', 'MailController@sendToAdmin')->name('sendToAdmin');
+    Route::get('/sendnew', 'MailController@sendUpdates')->name('sendUpdates')->middleware('admin');
+    Route::get('/regsubscr', 'MailController@regsubscr')->name('regsubscr');
+    Route::get('/regunsubscr', 'MailController@regunsubscr')->name('regunsubscr');
+    Route::get('/unsubscr/{email}', 'MailController@unsubscr')->name('unsubscr');
+    Route::post('/subscr', 'MailController@subscr')->name('subscr');
+    Route::post('/ajaxxrate', 'ProductController@ajaxxrate')->name('ajaxxrate');
+    Route::get('/checkout', 'ProductController@checkout')->name('checkout');
+    Route::any('/getfilteredcat', 'CategoryController@getFiltered')->name('getFilteredCat');
+    Route::get('/check', 'ProductController@check')->name('check');
+    Route::post('/formorder', 'ProductController@formorder')->name('formorder');*/
+
+
 });
